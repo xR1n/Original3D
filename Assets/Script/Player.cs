7b1c7f28e889ef54ed94b899a4a864a4e72db2a9
xR1n <105471170+xR1n@users.noreply.github.com>
2024-07-25 12:08:51 +0900

    追加

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    CharacterController charaControll;
-    Animation anim;
+    public Animation anim;
+    public CharacterController characterControll;
 
-    float normalSpeed = 3f; // 通常移動速度
-    float sprintSpeed = 6f; // ダッシュ移動速度
-    float jump = 7f;        // ジャンプ力
-    float gravity = 10f;    // 重力の大きさ
+    public float normalSpeed = 3f; // 通常移動速度
+    public float sprintSpeed = 6f; // ダッシュ移動速度
+    public float jump = 7f;        // ジャンプ力
+    public float gravity = 10f;    // 重力の大きさ
 
     Vector3 moveDirection = Vector3.zero;
     Vector3 startPos;
@@ -19,12 +19,23 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterControll = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 移動入力取得
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        characterControll.Move(moveDirection * normalSpeed * Time.deltaTime);
+
+        //// キャラクターの回転
+        //if(moveDirection !=Vector3.zero)
+        //{
+        //    Quaternion targetRotation = Quaternion.LookRotation(move);
+
+        //}
     }
 }
